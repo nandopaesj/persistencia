f3e29aa9c804335699be1c1db801a9036622a1b0
Aluno <etecae023@hotmail.com>
2025-03-11 20:23:31 -0300

    Trabalho concluído de consulta de clientes

--- database.sql
+++ database.sql
@@ -3,24 +3,59 @@ CREATE DATABASE IF NOT EXISTS AULA;
 USE AULA;
 
 CREATE TABLE IF NOT EXISTS cliente (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    NOME VARCHAR(100) NOT NULL,
-    EMAIL VARCHAR(100) NOT NULL,
-    TELEFONE VARCHAR(20),
-    ENDERECO VARCHAR(255)
-);  
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
+    endereco VARCHAR(255)
+);
 
 DELIMITER //
 
-CREATE PROCEDURE inserirCliente(
+CREATE PROCEDURE inserirCliente (
     IN p_nome VARCHAR(100),
     IN p_email VARCHAR(100),
-    IN p_telefone VARCHAR(100),
-    IN p_endereco VARCHAR(100)
+    IN p_telefone VARCHAR(20),
+    IN p_endereco VARCHAR(255)
 )
-BEGIN 
-    INSERT INTO cliente(nome, email, telefone, endereco)
-    VALUES(p_nome, p_email, p_telefone, p_endereco);
+BEGIN
+    INSERT INTO cliente (nome, email, telefone, endereco)
+    VALUES (p_nome, p_email, p_telefone, p_endereco);
+END //
+
+DELIMITER //
+
+CREATE PROCEDURE consultarCliente (
+    IN p_nome VARCHAR(100)
+)
+BEGIN
+    SELECT * 
+    FROM cliente
+    WHERE nome LIKE CONCAT('%', p_nome, '%');
+END //
+
+DELIMITER //
+
+CREATE PROCEDURE deletarCliente(
+    IN clienteID INT
+)
+BEGIN
+    DELETE FROM cliente WHERE id = clienteID;
+END //
+
+DELIMITER //
+
+CREATE PROCEDURE atualizarCliente(
+    IN p_id INT,
+    IN p_nome VARCHAR(255),
+    IN p_email VARCHAR(255),
+    IN p_telefone VARCHAR(20),
+    IN p_endereco VARCHAR(255)
+)
+BEGIN
+    UPDATE cliente
+    SET nome = p_nome, email = p_email, telefone = p_telefone, endereco = p_endereco
+    WHERE id = p_id;
 END //
 
 DELIMITER ;
--- server.js
+++ server.js
@@ -4,36 +4,36 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.use( express.static(path.join(__dirname, 'public')));
 
-// CONFIG A CONEXAO COM O BANCO DE DADOS
+app.use(express.static(path.join(__dirname, 'public')));
+
+//CONFIGURAR A CONEXAO COM O BANCO
 const connection = mysql.createConnection(
     {
         host:'127.0.0.1',
         user: 'root',
-        password: 'aluno',
-        database: 'aula',
+        password:'aluno',
+        database:'AULA',
         port: 3302
-    })
+    }
+)
 
-// CONECTAR COM O BANCO DE DADOS
-connection.connect( err => { 
+connection.connect(err => { 
     if(err){
-        console.error('Erro ao conectar ao banco', err);
+        console.error('Erro ao conectar ao banco. ', err);
         return;
     }
-    console.log('Conectado do banco');
- });
+    console.log('Conectado ao banco de dados!!!');
+  });
 
- // CONFIG DO BODY PARSER
- app.use( bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({extended: true}));
 
- // SERVIR O FORMULARIO HTML
- app.get( '/', (req, res)=>{ 
-    res.sendFile( path.join(__dirname, 'index.html'));
- });
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+ });    
 
- // PROCESSAR DADOS DO FORM
+
+//PROCESSAR DADOS DO FORMULARIO
  app.post('/inserirCliente', (req, res)=>{
     const{ nome, email, telefone, endereco } = req.body;
     const sql = 'CALL inserirCliente(?,?,?,?)';
@@ -47,8 +47,65 @@ connection.connect( err => {
     });
 });
 
-// INICIAR O SERVIDOR
-app.listen(port, () => {
-    console.log(`Servidor iniciando na em http://localhost:${port}`)
+ //ROTA PARA CONSULTAR CLIENTES
+ app.post('/consultarCliente', (req, res)=>{ 
+    const {nome} = req.body;
+
+    const sql = 'CALL consultarCliente(?)';
+
+    connection.query(sql, [nome], (err, results)=>{ 
+        if(err){
+            console.error('Erro ao consultar cliente: ', err);
+            res.send('Erro ao consultar cliente');
+            return;
+        }
+        const clientes = results[0];
+        res.send(`            
+            <script>
+                localStorage.setItem('clientes','${JSON.stringify(clientes)}');
+                localStorage.setItem('consultaRealizada', 'true');
+                window.location.href = '/consultarCliente.html';
+            </script>
+            
+            `);
+     });
+     
+   });
+
+
+//ROTA PARA EDITAR
+
+app.post('/editarCliente', (req, res)=>{
+    const {id, nome, email, telefone, endereco} = req.body;
+    const sql = 'CALL atualizarCliente(?,?,?,?,?)';
+
+    connection.query(sql, [id, nome, email, telefone, endereco ],
+         (err, results)=>{
+            if(err){
+                console.error('Erro ao editar cliente: ' , err);
+                res.send('Erro ao editar cliente. ');
+                return;
+            }
+            res.redirect('/consultarCliente.html?mensagem=editado');
+         });
+   });
+
+// ROTA DELETAR
+app.post('/deletarCliente', (req, res)=>{
+    const { id } = req.body;
+    const sql = 'CALL deletarCliente(?)';
+
+    connection.query(sql, [id], (err,results)=>{
+        if(err){
+            console.error('Erro ao deletar cliente', err);
+            res.send('Erro ao deletar cliente');
+            return;
+        }
+        res.redirect('/consultarCliente.html?mensagem=deletado');
+    });
 });
 
+// INICIAR O SERVIDOR
+app.listen(port, () => {
+    console.log(`Servidor iniciando http://localhost:${port}`)
+});
